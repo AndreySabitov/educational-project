15a484554325312606dd29e0c53dbbc404d1fa55
Andrey Sabitov <andrey.sabitoff@yandex.ru>
2025-10-27 17:29:09 +0500

    feat(TASK-731): Add task "Asynchronous error handling (.exceptionally())"

--- oop-collections-multithreading/asynchronous-programming/src/main/java/Main.java
+++ oop-collections-multithreading/asynchronous-programming/src/main/java/Main.java
@@ -1,12 +1,19 @@
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 
 public class Main {
     public static void main(String[] args) {
         ObjectMapper mapper = new ObjectMapper();
 
+        testCompletableFuture(mapper);
+
+        testExceptionally(mapper);
+    }
+
+    public static void testCompletableFuture(ObjectMapper mapper) {
         CompletableFuture.supplyAsync(() -> {
                     try {
                         Thread.sleep(1000);
@@ -14,6 +21,34 @@ public class Main {
                     } catch (InterruptedException | JsonProcessingException e) {
                         throw new RuntimeException(e);
                     }
+                }).thenApply(str -> {
+                    System.out.printf("Получили объект в формате JSON: %s%n", str);
+                    try {
+                        return mapper.readValue(str, Student.class);
+                    } catch (JsonProcessingException e) {
+                        throw new RuntimeException(e);
+                    }
+                }).thenAccept(student -> System.out.printf("Получили объект класса Student: %s%n", student))
+                .join();
+    }
+
+    public static void testExceptionally(ObjectMapper mapper) {
+        Random random = new Random();
+
+        CompletableFuture.supplyAsync(() -> {
+                    try {
+                        Thread.sleep(1000);
+                        int n = random.nextInt(501, 1000);
+                        if (n > 500) {
+                            throw new RuntimeException("Намеренное исключение");
+                        }
+                        return mapper.writeValueAsString(new Student("Roy", 18));
+                    } catch (InterruptedException | JsonProcessingException e) {
+                        throw new RuntimeException(e);
+                    }
+                }).exceptionally(throwable -> {
+                    System.err.println("Обработано исключение: " + throwable.getMessage());
+                    return "{}";
                 }).thenApply(str -> {
                     System.out.printf("Получили объект в формате JSON: %s%n", str);
                     try {
