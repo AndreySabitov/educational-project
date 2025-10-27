526f473cf40dd5469ed01520462775e4361d16ed
Andrey Sabitov <andrey.sabitoff@yandex.ru>
2025-10-27 15:24:19 +0500

    feat(TASK-731): Add task "Modern multithreading (ExecutorService)"

--- oop-collections-multithreading/multithreading-basics/src/main/java/Main.java
+++ oop-collections-multithreading/multithreading-basics/src/main/java/Main.java
@@ -3,14 +3,23 @@ import downloader.Downloader;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         List<Downloader> downloaderList = List.of(
                 new Downloader(), new Downloader(), new Downloader(), new Downloader(), new Downloader()
         );
+
+        testThreadAndRunnable(downloaderList);
+
+        testExecutorService(downloaderList);
+    }
+
+    public static void testThreadAndRunnable(List<Downloader> downloaderList) throws InterruptedException {
         long time1 = System.nanoTime();
-        for (Downloader downloader: downloaderList) {
+        for (Downloader downloader : downloaderList) {
             downloader.run();
         }
         long time2 = System.nanoTime();
@@ -18,15 +27,25 @@ public class Main {
 
         List<Thread> threads = new ArrayList<>();
         long time3 = System.nanoTime();
-        for (Downloader downloader: downloaderList) {
+        for (Downloader downloader : downloaderList) {
             Thread thread = new Thread(downloader);
             thread.start();
             threads.add(thread);
         }
-        for (Thread thread: threads) {
+        for (Thread thread : threads) {
             thread.join();
         }
         long time4 = System.nanoTime();
         System.out.printf("Время параллельного выполнения = %d%n", Duration.ofNanos(time4 - time3).toMillis());
+    }
+
+    public static void testExecutorService(List<Downloader> downloaderList) {
+        try (ExecutorService executorService = Executors.newFixedThreadPool(3)) {
+            for (Downloader downloader: downloaderList) {
+                executorService.submit(downloader);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
