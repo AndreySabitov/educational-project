4681fde2bbad6bf0aef2706d4eda69b88e2ae40d
Andrey Sabitov <andrey.sabitoff@yandex.ru>
2025-10-31 12:06:10 +0500

    feat(TASK-737): Add @PostConstruct and @PreDestroy to MessageService

--- intro-to-spring-framework/src/main/java/ru/sabitov/intro_to_spring_framework/service/MessageService.java
+++ intro-to-spring-framework/src/main/java/ru/sabitov/intro_to_spring_framework/service/MessageService.java
@@ -1,10 +1,14 @@
 package ru.sabitov.intro_to_spring_framework.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.sabitov.intro_to_spring_framework.repository.MessageRepository;
 
+@Slf4j
 @Service
 public class MessageService {
     private final MessageRepository messageRepository;
@@ -24,5 +28,15 @@ public class MessageService {
 
     public String getMessageFromRepository() {
         return messageRepository.getMessage();
+    }
+
+    @PostConstruct
+    private void postConstruct() {
+        log.info("Hello from postConstruct!");
+    }
+
+    @PreDestroy
+    private void preDestroy() {
+        log.info("Hello from preDestroy!");
     }
 }
