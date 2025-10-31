56bc1a628880343641d9bd344962b935da704c2b
Andrey Sabitov <andrey.sabitoff@yandex.ru>
2025-10-31 12:01:39 +0500

    feat(TASK-737): Create ObjectMapper bean

---
+++ intro-to-spring-framework/src/main/java/ru/sabitov/intro_to_spring_framework/config/ObjectMapperConfig.java
@@ -0,0 +1,14 @@
+package ru.sabitov.intro_to_spring_framework.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+}
--- intro-to-spring-framework/src/main/java/ru/sabitov/intro_to_spring_framework/service/MessageService.java
+++ intro-to-spring-framework/src/main/java/ru/sabitov/intro_to_spring_framework/service/MessageService.java
@@ -1,5 +1,6 @@
 package ru.sabitov.intro_to_spring_framework.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.sabitov.intro_to_spring_framework.repository.MessageRepository;
@@ -7,11 +8,14 @@ import ru.sabitov.intro_to_spring_framework.repository.MessageRepository;
 @Service
 public class MessageService {
     private final MessageRepository messageRepository;
+    private final ObjectMapper objectMapper;
+
     @Value("${app.greeting}")
     private String greeting;
 
-    public MessageService(MessageRepository messageRepository) {
+    public MessageService(MessageRepository messageRepository, ObjectMapper objectMapper) {
         this.messageRepository = messageRepository;
+        this.objectMapper = objectMapper;
     }
 
     public String getMessage() {
