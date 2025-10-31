50f3993fc433a41287e16c6724e73c751164f462
Andrey Sabitov <andrey.sabitoff@yandex.ru>
2025-10-31 10:48:55 +0500

    feat(TASK-737): Init Spring project

---
+++ intro-to-spring-framework/src/test/java/ru/sabitov/intro_to_spring_framework/IntroToSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sabitov.intro_to_spring_framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroToSpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
