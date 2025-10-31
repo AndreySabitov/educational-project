6b0988f9be6db525ede54c38d2b47055e1d86a79
Andrey Sabitov <andrey.sabitoff@yandex.ru>
2025-10-31 11:00:53 +0500

    feat(TASK-737): Add task "Read property from application.yaml"

--- intro-to-spring-framework/src/main/java/ru/sabitov/intro_to_spring_framework/controller/MessageController.java
+++ intro-to-spring-framework/src/main/java/ru/sabitov/intro_to_spring_framework/controller/MessageController.java
@@ -1,5 +1,6 @@
 package ru.sabitov.intro_to_spring_framework.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.sabitov.intro_to_spring_framework.service.MessageService;
 
@@ -9,5 +10,10 @@ public class MessageController {
 
     public MessageController(MessageService messageService) {
         this.messageService = messageService;
+    }
+
+    @GetMapping("/greeting")
+    public String getGreeting() {
+        return messageService.getMessage();
     }
 }
--- intro-to-spring-framework/src/main/java/ru/sabitov/intro_to_spring_framework/service/MessageService.java
+++ intro-to-spring-framework/src/main/java/ru/sabitov/intro_to_spring_framework/service/MessageService.java
@@ -1,11 +1,14 @@
 package ru.sabitov.intro_to_spring_framework.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageService {
+    @Value("${app.greeting}")
+    private String greeting;
 
     public String getMessage() {
-        return "";
+        return greeting;
     }
 }
