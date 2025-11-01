365e4ad9a670211166008ae17dd6e457b3633592
Andrey Sabitov <andrey.sabitoff@yandex.ru>
2025-11-01 09:59:19 +0500

    feat(TASK-739): Add findById endpoint to BookController

--- spring-rest-api/src/main/java/ru/sabitov/example/controller/BookController.java
+++ spring-rest-api/src/main/java/ru/sabitov/example/controller/BookController.java
@@ -1,14 +1,24 @@
 package ru.sabitov.example.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import ru.sabitov.example.model.Book;
 
 @RestController
+@Slf4j
 public class BookController {
 
     @GetMapping("/book")
     public Book getBook() {
-        return new Book("Book", "Author");
+        return new Book(1L,"Book", "Author");
+    }
+
+    @GetMapping("/books/{id}")
+    public Book findById(@PathVariable Long id) {
+        log.info("Поступил запрос на поиск книги с id = {}", id);
+
+        return new Book(id,"Book", "Author");
     }
 }
--- spring-rest-api/src/main/java/ru/sabitov/example/model/Book.java
+++ spring-rest-api/src/main/java/ru/sabitov/example/model/Book.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class Book {
+    private Long id;
     private String title;
     private String author;
 }
