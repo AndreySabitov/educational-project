ba2eb29ee6390f3262fbeb1aa1e60b56b4d85ecd
Andrey Sabitov <andrey.sabitoff@yandex.ru>
2025-11-19 17:55:32 +0500

    feat(TASK-747): Add login endpoint

--- spring-data-jpa/src/main/java/ru/sabitov/example/config/SecurityConfig.java
+++ spring-data-jpa/src/main/java/ru/sabitov/example/config/SecurityConfig.java
@@ -3,7 +3,8 @@ package ru.sabitov.example.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,8 +19,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
                         .anyRequest().authenticated())
 
-                .csrf(c -> c.ignoringRequestMatchers("/api/auth/**"))
-                .httpBasic(Customizer.withDefaults());
+                .csrf(c -> c.ignoringRequestMatchers("/api/auth/**"));
 
         return http.build();
     }
@@ -27,5 +27,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
--- spring-data-jpa/src/main/java/ru/sabitov/example/controller/AuthController.java
+++ spring-data-jpa/src/main/java/ru/sabitov/example/controller/AuthController.java
@@ -2,11 +2,15 @@ package ru.sabitov.example.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.sabitov.example.dto.LoginRequest;
 import ru.sabitov.example.dto.RegisterRequest;
 import ru.sabitov.example.dto.UserDto;
 import ru.sabitov.example.service.AuthService;
@@ -14,11 +18,22 @@ import ru.sabitov.example.service.AuthService;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@Slf4j
 public class AuthController {
     private final AuthService authService;
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping
     public ResponseEntity<UserDto> register(@Valid @RequestBody RegisterRequest request) {
+        log.info("Запрос на регистрацию пользователя {}", request.getUsername());
         return ResponseEntity.ok(authService.saveUser(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(@Valid @RequestBody LoginRequest request) {
+        log.info("Запрос на вход от пользователя {}", request.getUsername());
+        var token = new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword());
+        authenticationManager.authenticate(token);
+        return ResponseEntity.ok().build();
     }
 }
