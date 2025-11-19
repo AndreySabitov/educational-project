ab70f61d6eda483cbb3eb23d192fa5cd988519aa
Andrey Sabitov <andrey.sabitoff@yandex.ru>
2025-11-19 16:24:34 +0500

    feat(TASK-747): Add security filter chain

---
+++ spring-data-jpa/src/main/java/ru/sabitov/example/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package ru.sabitov.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request -> request
+                .requestMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
+                .anyRequest().authenticated())
+
+                .csrf(c -> c.ignoringRequestMatchers("/api/auth/**"));
+
+        return http.build();
+    }
+}
---
+++ spring-data-jpa/src/main/java/ru/sabitov/example/controller/AuthController.java
@@ -0,0 +1,16 @@
+package ru.sabitov.example.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @PostMapping
+    public ResponseEntity<String> register() {
+        return ResponseEntity.ok("OK");
+    }
+}
