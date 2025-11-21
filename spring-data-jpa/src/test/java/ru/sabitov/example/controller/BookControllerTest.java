c5997343b5a7a885faa2963c113783350cf727ac
Andrey Sabitov <andrey.sabitoff@yandex.ru>
2025-11-21 14:40:24 +0500

    feat(TASK-749): Add BookControllerTest

---
+++ spring-data-jpa/src/test/java/ru/sabitov/example/controller/BookControllerTest.java
@@ -0,0 +1,48 @@
+package ru.sabitov.example.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.sabitov.example.dto.BookDto;
+import ru.sabitov.example.jwt.JwtTokenProvider;
+import ru.sabitov.example.service.BookService;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Year;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = BookController.class)
+class BookControllerTest {
+    @Autowired
+    private MockMvc mvc;
+    @MockitoBean
+    private BookService bookService;
+    @MockitoBean
+    private JwtTokenProvider jwtTokenProvider;
+    @MockitoBean
+    private UserDetailsService userDetailsService;
+
+    private final BookDto bookDto = new BookDto(1L, "Title", "Author", Year.of(2002));
+
+    @Test
+    @WithMockUser
+    void testGetByIdEndpoint() throws Exception {
+        when(bookService.findById(1L)).thenReturn(bookDto);
+
+        mvc.perform(get("/books/{id}", 1L)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("Title"));
+    }
+
+}
