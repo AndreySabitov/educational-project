f9fd5baf21f947b962efb06f0538dcc261457159
Andrey Sabitov <andrey.sabitoff@yandex.ru>
2025-11-24 11:51:03 +0500

    feat(TASK-749): Add '@BeforeEach' example

--- java-and-jvm-basics/refactoring-the-calculator-into-methods/src/test/java/MainTest.java
+++ java-and-jvm-basics/refactoring-the-calculator-into-methods/src/test/java/MainTest.java
@@ -1,8 +1,14 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+
+    @BeforeEach
+    void log() {
+        System.out.println("Вывожу сообщение перед каждым тестом");
+    }
 
     @Test
     void testAdd() {
