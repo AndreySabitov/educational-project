ed7a6ddc72c56613d1880574e2502bcba0ba8323
Andrey Sabitov <andrey.sabitoff@yandex.ru>
2025-12-11 16:39:41 +0500

    feat(TASK-765): Add CircuitBreaker and Retry annotations

--- spring-data-jpa/src/main/java/ru/sabitov/example/client/NotificationFeignClient.java
+++ spring-data-jpa/src/main/java/ru/sabitov/example/client/NotificationFeignClient.java
@@ -1,6 +1,8 @@
 package ru.sabitov.example.client;
 
 import feign.FeignException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +11,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 public interface NotificationFeignClient {
 
     @PostMapping("/notify")
+    @CircuitBreaker(name = "notificationService", fallbackMethod = "createNotificationFallback")
+    @Retry(name = "notificationService")
     ResponseEntity<Void> createNotification() throws FeignException;
 
+    @PostMapping("/notify")
+    default ResponseEntity<Void> createNotificationFallback(Throwable throwable) {
+        return ResponseEntity.ok().build();
+    }
 }
