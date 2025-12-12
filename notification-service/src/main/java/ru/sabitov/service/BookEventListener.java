8f9151e0eaa8819defc3ac8577164ba7ffccf4f9
Andrey Sabitov <andrey.sabitoff@yandex.ru>
2025-12-12 16:52:43 +0500

    feat(TASK-759): Create NotificationProcessor with async save in mongo

--- notification-service/src/main/java/ru/sabitov/service/BookEventListener.java
+++ notification-service/src/main/java/ru/sabitov/service/BookEventListener.java
@@ -1,25 +1,21 @@
 package ru.sabitov.service;
 
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import ru.sabitov.NotificationProcessor;
 import ru.sabitov.example.dto.BookCreatedEvent;
-import ru.sabitov.mapper.NotificationMapper;
-import ru.sabitov.repository.NotificationRepository;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class BookEventListener {
-    private final NotificationRepository notificationRepository;
-
-    private static final Logger log = LoggerFactory.getLogger(BookEventListener.class);
+    private final NotificationProcessor notificationProcessor;
 
     @KafkaListener(topics = "book_events", containerFactory = "bookEventKafkaListenerContainerFactory")
     public void consumeBookCreatedEvent(BookCreatedEvent bookCreatedEvent) {
         log.info("Приняли уведомление из kafka: {}", bookCreatedEvent);
-        notificationRepository.save(NotificationMapper.toEntity(bookCreatedEvent));
-        log.info("Записали уведомление в mongo");
+        notificationProcessor.sendToMongo(bookCreatedEvent);
     }
 }
